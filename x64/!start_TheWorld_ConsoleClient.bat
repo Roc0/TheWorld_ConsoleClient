8c136905b793ef1fcb7b9174a0bf2cf86dd33b43
Riccardo Marini <riccardo.marini65@live.com>
2020-09-04 09:30:14 +0200

    VS2019

--- x64/!start_TheWorld_ConsoleClient.bat
+++ x64/!start_TheWorld_ConsoleClient.bat
@@ -4,7 +4,7 @@ set curpath=%~dp0
 set KBE_ROOT=C:/TheWorld/KBEngine/kbengine/
 @rem if defined KBE_RES_PATH (echo %KBE_RES_PATH%) else 
 set KBE_RES_PATH=C:/TheWorld/KBEngine/kbengine/kbe/res/;C:/TheWorld/Client/TheWorld_Assets/;C:/TheWorld/Client/TheWorld_Assets/scripts/;C:/TheWorld/Client/TheWorld_Assets/res/
-set KBE_BIN_PATH=%curpath%/x64/
+set KBE_BIN_PATH=%curpath%
 
 @echo KBE_ROOT=%KBE_ROOT%
 @echo KBE_RES_PATH=%KBE_RES_PATH%
