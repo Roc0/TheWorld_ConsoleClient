c6ee790ca5ef41ab814e97f00eb8f266df815df2
Riccardo Marini <riccardo.marini65@gmail.com>
2020-11-28 12:59:29 +0100

    fix

--- x64/!start_TheWorld_ConsoleClient.bat
+++ x64/!start_TheWorld_ConsoleClient.bat
@@ -1,9 +1,9 @@
 @echo off
 set curpath=%~dp0
 @rem if defined KBE_ROOT (echo %KBE_ROOT%) else 
-set KBE_ROOT=C:/TheWorld/KBEngine/kbengine/
+set KBE_ROOT=D:/TheWorld/KBEngine/kbengine/
 @rem if defined KBE_RES_PATH (echo %KBE_RES_PATH%) else 
-set KBE_RES_PATH=C:/TheWorld/KBEngine/kbengine/kbe/res/;C:/TheWorld/Client/TheWorld_Assets/;C:/TheWorld/Client/TheWorld_Assets/scripts/;C:/TheWorld/Client/TheWorld_Assets/res/
+set KBE_RES_PATH=D:/TheWorld/KBEngine/kbengine/kbe/res/;D:/TheWorld/Client/TheWorld_Assets/;D:/TheWorld/Client/TheWorld_Assets/scripts/;D:/TheWorld/Client/TheWorld_Assets/res/
 set KBE_BIN_PATH=%curpath%
 
 @echo KBE_ROOT=%KBE_ROOT%
@@ -14,3 +14,4 @@ del /Q %KBE_BIN_PATH%\log.log
 
 cd /D %curpath%
 start TheWorld_ConsoleClient_d.exe
+pause
